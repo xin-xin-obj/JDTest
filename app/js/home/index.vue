e9f7186e2bd00d46c8902da6d8737986e4d66ca1
xin-xin-obj <1244201106@qq.com>
2018-11-03 18:24:30 +0800

    first push

---
+++ app/js/home/index.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+    <div :class="$style.home">
+        <h1>Home</h1>
+        <h2 :class="$style.test">123132</h2>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" module>
+  @import '../../css/reset.scss';
+  .home{
+    color : orange;
+    font-size: 80px;
+  }
+  .test{
+    color : red;
+    font-size: 40px;
+  }
+</style>
